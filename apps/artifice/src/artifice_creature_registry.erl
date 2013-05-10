790068ba5b96889ca47001ff0a926ef5e78d758d
Fredrik Kjellgren <frobben@gmail.com>
2013-05-10 16:22:47 +0200

    add creature registry

---
+++ apps/artifice/src/artifice_creature_registry.erl
@@ -0,0 +1,22 @@
+-module(artifice_creature_registry).
+
+-export([register/2, unregister/1, whereis/1, init/0]).
+
+%%% API -------------------------------------------------------------------------
+
+%% @doc Register a new creature
+register(Cid, Pid) ->
+    ets:insert(?MODULE, {Cid, Pid}).
+
+%% @doc Unregister a terminated creature
+unregister(Cid) ->
+    ets:delete(?MODULE, Cid).
+
+%% @doc Look up pid associated with Cid
+whereis(Cid) ->
+    ets:lookup(?MODULE, Cid).
+
+%% @doc Start up the creature registry
+init() ->
+    ets:new(?MODULE, [set, public, named_table, {read_concurrency, true}]).
+
